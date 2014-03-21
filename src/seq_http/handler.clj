b780ea3091a4d7327e1f2948caf0fcfccd794722
aslag <aslag@tehlulz.com>
2014-03-20 22:37:15 -0600

    finished really simplistic sequence generators

--- src/seq_http/handler.clj
+++ src/seq_http/handler.clj
@@ -6,17 +6,23 @@
             [ring.middleware.json :as middleware]))
 
 (defn init []
-  (println "seq_http is starting"))
+  (println "seq_http is starting..."))
 
 (defn destroy []
-  (println "seq_http is shutting down"))
+  (println "seq_http is shutting down..."))
 
+(defn to_int [n] (Integer/parseInt n))
+
+(defn fib [a b] (cons a (lazy-seq (fib b (+ b a)))))
+
+; TODO: handle exceptions; ill input "like curl -i http://localhost:3000/api/repeat/12/aa" yields 500 with stacktrace, yikes!)
+; TODO: handle input out of bounds
 (defroutes app-routes
   ; context for entire app
   (context "/api" []
-    (GET "/repeat" [x n] (response (str "repeat " x n "times")))
-    (GET "/count/:n" [n] (response (str "count to " n)))
-    (GET "/fib/:n" [n] (response (str "fibs to " n))))
+    (GET "/repeat/:x/:n" [n x] (response (repeat (to_int n) x)))
+    (GET "/count/:n" [n] (response (range (to_int n))))
+    (GET "/fib/:n" [n] (response (take (to_int n) (fib 0 1)))))
   (route/not-found "not found"))
 
 (def app
